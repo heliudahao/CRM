f31a7f9e88d7d1b33dc5c2d0cfb6fb1ee3e05195
zs <zs@bjpowernode.com>
2021-07-16 22:29:30 +0800

    第一次

---
+++ crm/src/test/java/com/bjpowernode/workbench/test/ActivityTest.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.workbench.test;
+
+import com.bjpowernode.crm.utils.SqlSessionUtil;
+import com.bjpowernode.crm.workbench.dao.ActivityDao;
+import com.bjpowernode.crm.workbench.domain.Activity;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ActivityTest {
+
+
+
+
+}
